4d7d20122bbb7eea95d57d458ec87721153ed4b6
Tangx2020 <tangx2020@163.com>
2020-12-05 20:05:28 +0800

    可以完成频率互不相等的情况，复杂情况有问题

---
+++ hufftree.h
@@ -0,0 +1,195 @@
+﻿#ifndef HUFFTREE_H
+#define HUFFTREE_H
+#include <iostream>
+#include <queue>
+#include <iomanip>
+#include <string>
+using namespace std;
+//有n个叶结点的树----2n-1个结点
+
+typedef struct
+{
+    int weight;
+    int parent,lchild,rchild;
+}HTNode,*HuffmanTree;
+
+
+typedef struct
+{
+    int pos;
+    char ch;
+    vector<int> charCode;
+}Node,*NodeList;
+
+void createHTree(HuffmanTree &HTree,int w[],int n)
+//w权值向量，n个字符，构造哈夫曼树HT
+{
+    if(n<=1)return;
+    int totalNum=2*n-1;
+    HTree=(HuffmanTree)malloc((totalNum+1)*sizeof (HTNode));
+    //输入哈夫曼树的初态,从下标为1开始输入，0号单元不使用
+    int i;
+    for(i=1;i<=n;i++)
+    {
+        HTree[i].weight=w[i-1];
+        HTree[i].parent=0;
+        HTree[i].lchild=0;
+        HTree[i].rchild=0;
+    }
+    for(;i<=totalNum;i++)
+    {
+        HTree[i].weight=0;
+        HTree[i].parent=0;
+        HTree[i].lchild=0;
+        HTree[i].rchild=0;
+    }//初态构造
+
+    priority_queue<int,vector<int>,greater<int> > myqueue;//优先队列，用来操作结点
+    for(int i=0;i<n;i++)
+    {
+        //将权值放到优先队列中
+        myqueue.push(w[i]);
+    }
+
+    //优先队列中的结点个数大于等于2时，将最小的两个元素记住，求和
+    //最小的两个元素出队，新生成的和入队
+    //在哈夫曼树中相应进行操作——两个最小结点---填写parent结点
+    //并且生成新的结点，权值为和，并填写其孩子结点
+    for(int current=n+1;current<=totalNum;current++)//current是当前正在构造的结点的位置
+    {
+        double minW1=myqueue.top();
+        myqueue.pop();
+        double minW2=myqueue.top();
+        myqueue.pop();
+        //得到队列中权值最小的两个权值，出队
+        double addW=minW1+minW2;
+        myqueue.push(addW);
+        int minPos1,minPos2;//最小权值的存储位置
+        for(int i=1;i<=totalNum;i++)
+        {
+            if(HTree[i].weight==minW1)
+            {
+                minPos1=i;
+                break;
+            }
+        }
+        for(int i=1;i<=totalNum;i++)
+        {
+            if(HTree[i].weight==minW2&&i!=minPos1)
+            {
+                minPos2=i;
+            }
+        }
+        //下面构造新结点
+        HTree[current].weight=addW;
+        HTree[minPos1].parent=current;
+        HTree[minPos2].parent=current;
+
+
+        HTree[current].lchild=minPos1;
+        HTree[current].rchild=minPos2;
+        //这样构造使得左子树小于右子树
+    }
+}
+void findCode(HuffmanTree &HTree,int n,int pos,vector<int> &charCode)
+{
+    //已经有哈夫曼树之后，给定某叶结点的位置，写出哈夫曼编码，放到charcode里面
+    int cur_pos=pos;
+    int find_parent=0;
+    while(1)
+    {
+        int i=0;
+        for(i=0;i<2*n;i++)
+        {
+            if(HTree[i].lchild==cur_pos)
+            {
+                find_parent=i;
+                //再记下哈夫曼编码
+                charCode.insert(charCode.begin(),0);//头插
+                cur_pos=find_parent;
+            }
+            else if(HTree[i].rchild==cur_pos)
+            {
+                find_parent=i;
+                charCode.insert(charCode.begin(),1);//头插
+                cur_pos=find_parent;
+            }
+        }
+        if(find_parent==2*n-1)
+        {
+            break;
+        }
+    }
+}
+void HuffmanCoding(HuffmanTree HTree,char charList[],int n,Node codeList[])
+//根据哈夫曼树，将所有字符的编码保存到codelist里面
+{
+    for(int i=1;i<=n;i++)//i是哈夫曼表里面的下标，从1开始
+    {
+        codeList[i].ch=charList[i-1];
+        findCode(HTree,n,i,codeList[i].charCode);
+    }
+}
+void PrintVector(vector<int> vec)
+{
+    int n=vec.size();
+    for(int i=0;i<n;i++)
+    {
+        cout<<vec[i];
+    }
+}
+void printAllCode(int n,Node codeList[])
+{
+    for(int i=1;i<=n;i++)
+    {
+        cout.flags(ios::left);
+        cout<<codeList[i].ch<<":  ";
+        PrintVector(codeList[i].charCode);
+        cout<<endl;
+    }
+}
+void passageCoding(vector<char> str,Node codelist[],int n)
+{
+    for(int i;;i++)
+    {
+        if(str[i]=='#')
+            break;
+        for(int j=1;j<=n;j++)
+        {
+            if(str[i]==codelist[j].ch)
+            {
+                PrintVector(codelist[j].charCode);
+                break;
+            }
+        }
+    }
+}
+bool checkLeaf(HuffmanTree Htree,int pos)
+{
+    if(Htree[pos].lchild==0&&Htree[pos].rchild==0)
+        return 1;
+    else return 0;
+}
+void decode(HuffmanTree HTree,int n,char charList[])
+//根据哈夫曼树进行解码,n是叶结点个数
+{
+    int curPos=2*n-1;//在哈夫曼树上遍历，从根结点开始往下走，走到叶结点又拉回根结点
+    char ch;
+    while(cin>>ch)
+    {
+        if(ch=='1')
+        {
+            curPos=HTree[curPos].rchild;
+        }
+        else if(ch=='0')
+        {
+            curPos=HTree[curPos].lchild;
+        }
+        if(checkLeaf(HTree,curPos))//走到了叶子结点
+        {
+            cout<<charList[curPos-1];
+            curPos=2*n-1;
+        }
+    }
+}
+#endif // HUFFTREE_H
---
+++ main.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <hufftree.h>
+using namespace std;
+#define MAX_CHAR_NUM 31
+
+
+int main()
+{
+
+    cout<<"please input a string, only contain letters, blanks or ',' '.' '?' '!' ,end with a '#'"<<endl;
+    char charList[MAX_CHAR_NUM];//a到z在0到25，26，27，28，29为标点符号,30为空格
+    for(int i=0;i<MAX_CHAR_NUM;i++)
+    {
+        if(i<26)
+        {
+            charList[i]=i+'a';
+        }
+        charList[26]=',';
+        charList[27]='.';
+        charList[28]='?';
+        charList[29]='!';
+        charList[30]=' ';
+    }
+
+    int weight[MAX_CHAR_NUM]={0};//计数，出现次数
+
+    vector<char> myinput;
+
+    for(int i=0;;i++)
+    {
+        char ch;
+        scanf("%c",&ch);
+        //cin>>ch;
+        myinput.push_back(ch);//将输入保存到数组里面
+
+        if(ch=='#')
+        {
+            break;
+        }
+
+        if(ch>='a'&&ch<='z')
+        {
+            weight[ch-'a']++;
+        }
+        else if(ch>='A'&&ch<='Z')
+        {
+            weight[ch-'A']++;
+        }
+        else if(ch==',')
+        {
+            weight[26]++;
+        }
+        else if(ch=='.')
+        {
+            weight[27]++;
+        }
+        else if(ch=='?')
+        {
+            weight[28]++;
+        }
+        else if(ch=='!')
+        {
+            weight[29]++;
+        }
+        else if(ch==' ')
+        {
+            weight[30]++;
+        }
+    }
+
+    int inputLength=myinput.size();
+    vector<char> newCharList;
+    vector<int> newWeight;
+    //去掉权重为0的字符
+    for(int i=0;i<MAX_CHAR_NUM;i++)
+    {
+        if(weight[i]!=0)
+        {
+            newWeight.push_back(weight[i]);
+            newCharList.push_back(charList[i]);
+        }
+    }
+    int n=newWeight.size();//权重不为零的字符个数
+    //int weightList[30];
+    for(int i=0;i<n;i++)
+    {
+        weight[i]=newWeight[i];
+        charList[i]=newCharList[i];
+    }
+    HuffmanTree HTree;
+    createHTree(HTree,weight,n);
+
+
+    cout<<"-------------------------------------------"<<endl;
+    cout<<"the HC chart is"<<endl;
+    cout.flags(ios::left);
+    cout<<setw(5)<<"pos"<<setw(5)<<"ch"<<setw(5)<<"ww"<<setw(5)<<"p"<<setw(5)<<"l"<<setw(5)<<"r"<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout.flags(ios::left);
+        cout<<setw(5)<<i<<setw(5)<<charList[i-1]<<setw(5)<<HTree[i].weight<<setw(5)<<HTree[i].parent<<setw(5)<<HTree[i].lchild<<setw(5)<<HTree[i].rchild<<endl;
+    }
+    for(int i=n+1;i<2*n;i++)
+    {
+        cout.flags(ios::left);
+        cout<<setw(5)<<i<<setw(5)<<"-"<<setw(5)<<setw(5)<<HTree[i].weight<<setw(5)<<HTree[i].parent<<setw(5)<<HTree[i].lchild<<setw(5)<<HTree[i].rchild<<endl;
+    }
+    cout<<"-------------------------------------------"<<endl;
+    //调试可以正确建立哈夫曼树
+
+
+    Node codeList[2*MAX_CHAR_NUM];
+    HuffmanCoding(HTree,charList,n,codeList);
+    cout<<endl;
+    cout<<"-------------------------------------------"<<endl;
+    cout<<"the Huffmancode is"<<endl;
+    printAllCode(n,codeList);
+    cout<<"-------------------------------------------"<<endl;
+    //输出每个字符的编码
+
+    cout<<"coding the input passage"<<endl;
+    passageCoding(myinput,codeList,n);//输出一开始输入的“文章”的编码
+    cout<<endl;
+    cout<<"-------------------------------------------"<<endl;
+    cout<<"input a string of Huffman code, using the coding method as afore-mentioned"<<endl;
+    cout<<"you can copy the code output above, and input it to see if it can be successfully decoded"<<endl;
+
+    decode(HTree,n,charList);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
